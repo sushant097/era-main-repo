df2f4d3ecc14ea763b3b4ad2377544cf3f31bbd1
sushant097 <sushant1234gautam@gmail.com>
2023-08-06 13:45:19 -0500

    added new files

---
+++ utils - Copy.py
@@ -0,0 +1,133 @@
+# CODE BLOCK: 1
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+from torch.optim.lr_scheduler import StepLR, ReduceLROnPlateau
+from torch.optim.lr_scheduler import OneCycleLR
+from tqdm import tqdm
+import numpy as np
+
+ 
+    
+# Code Block: 8
+
+# Data to plot accuracy and loss graphs
+train_losses = []
+test_losses = []
+train_acc = []
+test_acc = []
+
+test_incorrect_pred = {'images': [], 'ground_truths': [], 'predicted_vals': []}
+
+# Code Block: 9
+
+def GetCorrectPredCount(pPrediction, pLabels):
+  return pPrediction.argmax(dim=1).eq(pLabels).sum().item()
+
+def train(model, device, train_loader, optimizer, scheduler, criterion, epoch):
+  model.train()
+  pbar = tqdm(train_loader)
+
+  train_loss = 0
+  correct = 0
+  processed = 0
+  
+  if scheduler == 'StepLR':
+    scheduler = StepLR(optimizer, step_size=100, gamma=0.25)
+    sched_flag = True
+  elif scheduler == 'OneCycle':
+    scheduler = OneCycleLR(optimizer=optimizer, max_lr=1.05E-03, epochs=epoch, steps_per_epoch=len(train_loader), pct_start=5/epoch, div_factor=10) 
+    sched_flag = True
+  else:
+    sched_flag = False
+
+  for batch_idx, (data, target) in enumerate(pbar):
+    data, target = data.to(device), target.to(device)
+    optimizer.zero_grad()
+
+    # Predict
+    y_pred = model(data)
+
+    # Calculate loss
+    loss = criterion(y_pred, target)
+    train_loss+=loss.item()
+
+    # Backpropagation
+    loss.backward()
+    optimizer.step()
+    
+    if sched_flag:
+      scheduler.step()
+    
+    # Update pbar-tqdm
+
+    pred = y_pred.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+    correct += pred.eq(target.view_as(pred)).sum().item()
+    processed += len(data)
+    
+    pbar.set_description(desc= f'Loss={loss.item()} Batch_id={batch_idx} Accuracy={100*correct/processed:0.2f}')
+
+    pbar.set_description(desc= f'Train: Loss={loss.item():0.4f} Batch_id={batch_idx} Accuracy={100*correct/processed:0.2f}')
+
+  train_acc.append(100*correct/processed)
+  train_losses.append(train_loss/len(train_loader))
+
+def test(model, device, test_loader,  criterion ):
+    model.eval()
+
+    test_loss = 0
+    correct = 0
+
+    with torch.no_grad():
+        for batch_idx, (data, target) in enumerate(test_loader):
+            data, target = data.to(device), target.to(device)
+
+            output = model(data)
+            test_loss += criterion(output, target).item()  # sum up batch loss
+
+            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+
+    test_loss /= len(test_loader.dataset)
+    test_acc.append(100. * correct / len(test_loader.dataset))
+    test_losses.append(test_loss)
+
+    print('Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+     
+
+
+# Code Block 11:
+
+def plot_losses():
+  fig, axs = plt.subplots(2,2,figsize=(15,10))
+  axs[0, 0].plot(train_losses)
+  axs[0, 0].set_title("Training Loss")
+  axs[1, 0].plot(train_acc)
+  axs[1, 0].set_title("Training Accuracy")
+  axs[0, 1].plot(test_losses)
+  axs[0, 1].set_title("Test Loss")
+  axs[1, 1].plot(test_acc)
+  axs[1, 1].set_title("Test Accuracy")
+  plt.show()
+  
+  
+
+def plot_incorrect_prediction(mismatch, n=10 ):
+    classes = ['plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+    display_images = mismatch[:n]
+    index = 0
+    fig = plt.figure(figsize=(10,5))
+    for img in display_images:
+        image = img[0].squeeze().to('cpu').numpy()
+        pred = classes[img[1]]
+        actual = classes[img[2]]
+        ax = fig.add_subplot(2, 5, index+1)
+        ax.axis('off')
+        ax.set_title(f'\n Predicted Label : {pred} \n Actual Label : {actual}',fontsize=10) 
+        ax.imshow(np.transpose(image, (1, 2, 0))) 
+        index = index + 1
+    plt.show()
